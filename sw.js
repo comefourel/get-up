811d58415d3ed41bcf0b3bd8ca751083309d4662
comefourel <comefourel@gmail.com>
2026-08-31 15:12:03 +0200

    sw.js

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@
    les fichiers sur GitHub. Sans ça, ton iPhone continuera de
    servir l'ancienne version depuis son cache.
    ============================================================ */
-const VERSION = 'v4';
+const VERSION = 'v6';
 
 const SHELL = `semaine-shell-${VERSION}`;
 const FONTS = `semaine-fonts-${VERSION}`;
